4f0bfe4a824a8ddb55b6ad721e4ea7a09968471f
Emily0Walker <emic.walker@gmail.com>
2024-07-10 15:24:05 -0600

    main program

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        Console.WriteLine("Welcome to Eternal Quest");
+        Console.WriteLine("1. create goal ) ");
+        Console.WriteLine("2. check goal ) ");
+        Console.WriteLine("3. save goal) ");
+        Console.WriteLine("4. load goal) ");
+        Console.WriteLine("Choose your Goal: ");
+        string choice = Console.ReadLine();
+
+        if (choice == "1")
+        {
+
+        }
+        else if (choice == "2")
+        {
+
+        }
+        else if (choice == "3")
+        {
+
+        }
+        else if (choice == "4")
+        {
+
+        }
+
     }
 }
