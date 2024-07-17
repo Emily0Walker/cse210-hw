7e8ca4f8ae32fa8e1ae5d4163ba59edb85d1b70c
Emily0Walker <emic.walker@gmail.com>
2024-07-17 13:04:20 -0600

    i forgot

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -1,37 +1,37 @@
 // Comment.cs
 public class Comment
 {
-    private string name;
-    private string text;
+    private string Name;
+    private string Text;
 
     // Constructor to initialize comment properties
     public Comment(string name, string text)
     {
-        this.name = name;
-        this.text = text;
+        Name = name;
+        Text = text;
     }
 
     // Method to get the name
     public string GetName()
     {
-        return name;
+        return Name;
     }
 
     // Method to set the name
     public void SetName(string name)
     {
-        this.name = name;
+        Name = name;
     }
 
     // Method to get the text of the comment
     public string GetText()
     {
-        return text;
+        return Text;
     }
 
     // Method to set the text of the comment
     public void SetText(string text)
     {
-        this.text = text;
+        Text = text;
     }
 }
